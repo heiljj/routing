e6bc8f855cc0c31f618fa1e39206ee5d55481942
heiljj <heiljj@rose-hulman.edu>
2026-04-06 23:00:09 -0500

    span routing

--- spanmap.py
+++ spanmap.py
@@ -3,81 +3,56 @@ The initial network starts from a logic cell output."""
 import itertools
 from icestorm.icebox.icebox import iceconfig
 
-OUT_FILE = "spanmap.txt"
-
 icebox = iceconfig()
 icebox.setup_empty_5k()
 
+def generate_lookup(tile: tuple[int, int]) -> dict[str, list[str]]:
+    nets = icebox.tile_db(*tile)
+    nets = filter(lambda x : x[1] in ("buffer", "routing"), nets)
+    # src, dst, config
+    nets = [(x[2], x[3], tuple(x[0])) for x in nets]
+    nets = filter(lambda x: "sp" in x[0] and "sp" in x[1], nets)
 
-# follow_net only works on external nets
-nets = icebox.tile_db(15, 15)
-nets = filter(lambda x : x[1] in ("buffer", "routing"), nets)
-# src, dst
-nets = [(x[2], x[3]) for x in nets]
-nets = filter(lambda x: all("sp" in y for y in x), nets)
+    # src -> dst, bitconfig
+    return {k: [x[1:] for x in i] for k, i in itertools.groupby(nets, key=lambda x : x[0])}
 
-# src -> dst's
-internal_lookup = {k: [x[1] for x in i] for k, i in itertools.groupby(nets, key=lambda x : x[0])}
+class NoPathFoundException(Exception): ...
 
-def route(target_tile: tuple[int, int], start_tile: tuple[int, int], src_net: str, disallowed_nets: list[str], disallowed_tiles: list[tuple[int, int]], depth=10) -> list[tuple[int, int, str]]:
+def route(target_tile: tuple[int, int], start_tile: tuple[int, int], src_net: str, disallowed_nets: list[str], disallowed_tiles: list[tuple[int, int]], depth=40) -> list[tuple[int, int, str]]:
     """Route span in start tile to span in target tile using only span to span connections. Ignores disallowed nets and nets crossing into
     disallowed tiles. Returns path of connections as [(x, y, net_name)], where net_name is named relative to the previous entry
     (the first entry is named relative to the starting tile)."""
-    search_paths = [[(*start_tile, src_net)]]
+    internal_lookup = generate_lookup(start_tile)
+    search_paths = [[(*start_tile, src_net, None)]]
 
     visited_locations = set()
 
     for _ in range(depth):
         next_paths = []
         for path in search_paths:
-            for x, y, net in icebox.follow_net(path[-1]):
+            for x, y, net, in icebox.follow_net(path[-1][:3]):
                 if (x, y) in disallowed_tiles or net in disallowed_nets:
                     continue
 
-                if (x, y, net) in visited_locations:
+                if (x, y, net, None) in visited_locations:
                     continue
 
-                visited_locations.add((x, y, net))
+                visited_locations.add((x, y, net, None))
 
                 if target_tile == (x, y):
-                    return path + [(x, y, net)]
+                    return path + [(x, y, net, None)]
 
-                next_paths.append(path + [(x, y, net)])
+                next_paths.append(path + [(x, y, net, None)])
 
-            x, y, net = path[-1]
-            for buffer in internal_lookup.get(net, []):
-                if (x, y, buffer) in visited_locations:
+            x, y, net, _ = path[-1]
+            for buffer, config in internal_lookup.get(net, []):
+                if (x, y, buffer, config) in visited_locations:
                     continue
 
-                visited_locations.add((x, y, buffer))
+                visited_locations.add((x, y, buffer, config))
 
-                next_paths.append(path + [(x, y, buffer)])
+                next_paths.append(path + [(x, y, buffer, config)])
 
         search_paths = next_paths
 
-    raise IndentationError()
-
-def find_tiles(src):
-    nets = icebox.tile_db(*src)
-    nets = filter(lambda x : len(x) == 4, nets)
-    nets = filter(lambda x : "out" in x[2], nets)
-    nets = filter(lambda x : "sp" in x[3], nets)
-    nets = [x[3] for x in nets]
-
-    for x in range(1, 25):
-        for y in range(1, 31):
-            if src == (x, y):
-                continue
-
-            for net in nets:
-                try:
-                    path = route((x, y), src, net, [], [], depth=50)
-                    with open(OUT_FILE, "a") as f:
-                        f.write(f"({src[0]}, {src[1]}) -> ({x}, {y}) (len {len(path)})\n")
-                    break
-                except IndentationError:
-                    pass
-
-    return False
-
-find_tiles((15, 15))
+    raise NoPathFoundException()
